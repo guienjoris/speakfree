15bdfdf8290a3ddc8df68df4f7630010092764e1
guienjoris <guienjoris@gmail.com>
2020-01-24 15:35:44 +0100

    update

--- server/server.js
+++ server/server.js
@@ -25,7 +25,6 @@ mongoose.connect(urlmongo, {
     useFindAndModify:false,
     useCreateIndex:true
 });
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console,'connection error'));
 db.once('open', ()=>{
